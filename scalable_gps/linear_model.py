bffdbbae391211f4f2b865fb1548c5f58fc973b4
Javier Antoran <javir803@gmail.com>
2023-03-26 16:30:36 +0100

    placed things in an importable package, implemented batched KvP and added toy ntebook

--- scalable_gps/linear_model.py
+++ scalable_gps/linear_model.py
@@ -2,12 +2,15 @@ import jax
 import jax.numpy as jnp
 import jax.random as jr
 from chex import Array, PRNGKey
-from utils import TargetTuple
+from .utils import TargetTuple
 from typing import Callable, Optional
+import chex
+
 
 def error(params, targets, K):
     # TODO: Make this not always take in instantiated kernel, but RFF features etc.
-    return .5 * jnp.sum((targets - K @ params) ** 2)
+    return 0.5 * jnp.sum((targets - K @ params) ** 2)
+
 
 # TODO: pmap over B.
 # TODO: pass idx directly, can be different sizes depending on pmap and vmap.
@@ -16,29 +19,37 @@ def error_grad_sample(params, key, B, x, target, kernel_fn):
     N = x.shape[0]
     idx = jr.randint(key, shape=(B,), minval=0, maxval=N)
     K = kernel_fn(x[idx], x)
-    return -K.T @ (target[idx] - K @ params) * (N / B)
+    datapoint_grads = -K.T @ (target.squeeze()[idx] - K @ params) * (N / B)
+    return datapoint_grads
 
 
 def regularizer(params, target, K, noise_scale):
-    params = (noise_scale ** 2) * params
-    return .5 * (params - target).T @ K @ (params - target)
+    params = (noise_scale**2) * params
+    target = target.squeeze()
+    return 0.5 * (params - target).T @ K @ (params - target)
 
 
-# TODO: pmap over M.
-def regularizer_grad_sample(params, key, M, x, target, feature_fn, noise_scale, recompute_features=True):
+def regularizer_grad_sample(
+    params, key, M, x, target, feature_fn, noise_scale, recompute_features=True
+):
     R = feature_fn(key, M, x, recompute=recompute_features)
-    params = (noise_scale ** 2) * params
-    return R @ (R.T @ (params - target))
+    params = (noise_scale**2) * params
+    return R @ (R.T @ (params - target.squeeze()))
 
 
 def loss_fn(params: Array, target_tuple: TargetTuple, K: Array, noise_scale):
     # TODO: MAke the loss function not always take in instantiated kernel, but RFF features etc.
-    return error(params, target_tuple.error_target, K) + regularizer(params, target_tuple.regularizer_target, K, noise_scale)
+    return (
+        error(params, target_tuple.error_target, K).squeeze()
+        + regularizer(params, target_tuple.regularizer_target, K, noise_scale).squeeze()
+    )
 
 
 def exact_solution(targets, K, noise_scale):
     # TODO: Add jax.scipy.linalg.solve with pos_sym=True
-    return jnp.linalg.solve(K + (noise_scale ** 2) * jnp.identity(targets.shape[0]), targets)
+    return jnp.linalg.solve(
+        K + (noise_scale**2) * jnp.identity(targets.shape[0]), targets
+    )
 
 
 def predict(params, x_pred, x_train, kernel_fn, **kernel_kwargs):
@@ -46,8 +57,15 @@ def predict(params, x_pred, x_train, kernel_fn, **kernel_kwargs):
 
 
 def draw_prior_function_sample(
-    feature_key: PRNGKey, prior_function_key: PRNGKey, M: int, x: Array, feature_fn: Callable, K: Optional[Array]=None, 
-    use_chol: bool = False, chol_eps: float = 1e-5):
+    feature_key: chex.PRNGKey,
+    prior_function_key: chex.PRNGKey,
+    M: int,
+    x: Array,
+    feature_fn: Callable,
+    K: Optional[Array] = None,
+    use_chol: bool = False,
+    chol_eps: float = 1e-5,
+):
     """Returns prior fn sample along with either cholesky factorization of K or feature matrix R."""
     N = x.shape[0]
     if use_chol:
@@ -62,5 +80,3 @@ def draw_prior_function_sample(
 
 def draw_prior_noise_sample(prior_noise_key, N, noise_scale):
     return noise_scale * jr.normal(prior_noise_key, (N,))
-            
-    
--- scalable_gps/metrics.py
+++ scalable_gps/metrics.py
@@ -1,27 +1,30 @@
-
 import jax
 import jax.random as jr
 import jax.numpy as jnp
 from chex import Array, PRNGKey
 from typing import Optional
-from utils import revert_z_score
+from .utils import revert_z_score
 from typing import Callable
+import chex
 
 
-def grad_var_fn(params: Array, grad_fn: Callable, num_evals: int = 100, key: PRNGKey=jr.PRNGKey(12345)):
+def grad_var_fn(
+    params: Array,
+    grad_fn: Callable,
+    num_evals: int = 100,
+    key: chex.PRNGKey = jr.PRNGKey(12345),
+):
     grad_var_key = jr.split(key, num_evals)
     grad_samples = jax.vmap(grad_fn, (None, 0))(params, grad_var_key)
     grad_var = jnp.var(grad_samples, axis=0).mean()
-    
+
     return grad_var
 
 
-def RMSE(x: Array, x_hat: Array, mu: Optional[Array]=None, sigma: Optional[Array]=None):
+def RMSE(
+    x: Array, x_hat: Array, mu: Optional[Array] = None, sigma: Optional[Array] = None
+):
     if mu is not None and sigma is not None:
         x = revert_z_score(x, mu, sigma)
         x_hat = revert_z_score(x_hat, mu, sigma)
     return jnp.sqrt(jnp.mean((x - x_hat) ** 2))
-
-
-
-
