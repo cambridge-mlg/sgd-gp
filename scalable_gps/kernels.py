3950a7c80de0f9456ef8e32505440a3fe883cd33
shreyaspadhy <shreyaspadhy@gmail.com>
2023-03-27 16:52:04 +0000

    matern 3/2 and RFF works

--- scalable_gps/kernels.py
+++ scalable_gps/kernels.py
@@ -1,9 +1,10 @@
+from functools import partial
+
+import chex
 import jax
 import jax.numpy as jnp
 import jax.random as jr
-from functools import partial
 from chex import Array
-import chex
 
 # TODO: Implement Matern 3/2 and Laplace
 # TODO: Implement RFF for Matern 3/2 - https://github.com/secondmind-labs/GPflux/blob/3833993ccf2e6e166dd02a1bdb7c9778d3385318/gpflux/layers/basis_functions/fourier_features/random.py#L71
@@ -25,30 +26,16 @@ class Kernel:
 
     def K(self, x: Array, y: Array):
         raise NotImplementedError("Subclasses should implement this method.")
-
-    def Phi(self, key: chex.PRNGKey, n_features: int, x, recompute: bool = False):
+    
+    def omega_fn(self, key: chex.PRNGKey, num_input_dims: int, num_features: int):
         raise NotImplementedError("Subclasses should implement this method.")
-
+    
     def phi_fn(self, key: chex.PRNGKey, num_features: int):
         return jr.uniform(key, shape=(1, num_features), minval=-jnp.pi, maxval=jnp.pi)
-
-
-class RBFKernel(Kernel):
-    @partial(jax.jit, static_argnums=(0,))
-    def K(self, x: Array, y: Array):
-
-        self.check_required_hparams_in_config(
-            ["signal_scale", "length_scale"], self.kernel_config
-        )
-
-        s = self.kernel_config["signal_scale"]
-        l = self.kernel_config["length_scale"]
-        d2 = jnp.sum((x[:, None] - y[None, :]) ** 2, axis=-1)
-        return (s**2) * jnp.exp(-0.5 * d2 / (l**2))
-
-    def omega_fn(self, key: chex.PRNGKey, num_input_dims: int, num_features: int):
-        return jr.normal(key, shape=(num_input_dims, num_features))
-
+    
+    def _sq_dist(self, x: Array, y: Array):
+        return jnp.sum((x[:, None] - y[None, :]) ** 2, axis=-1)
+    
     def Phi(
         self, key: chex.PRNGKey, n_features: int, x: Array, recompute: bool = False
     ):
@@ -70,3 +57,42 @@ class RBFKernel(Kernel):
             omega, phi = self.omega, self.phi
 
         return s * jnp.sqrt(2.0 / M) * jnp.cos(x @ (omega / l) + phi)
+
+
+class RBFKernel(Kernel):
+    @partial(jax.jit, static_argnums=(0,))
+    def K(self, x: Array, y: Array):
+
+        self.check_required_hparams_in_config(
+            ["signal_scale", "length_scale"], self.kernel_config
+        )
+
+        s = self.kernel_config["signal_scale"]
+        l = self.kernel_config["length_scale"]
+
+        return (s**2) * jnp.exp(-0.5 * self._sq_dist(x, y) / (l**2))
+
+    def omega_fn(self, key: chex.PRNGKey, num_input_dims: int, num_features: int):
+        return jr.normal(key, shape=(num_input_dims, num_features))
+
+
+class Matern32Kernel(Kernel):
+    @partial(jax.jit, static_argnums=(0,))
+    def K(self, x: Array, y: Array):
+
+        self.check_required_hparams_in_config(
+            ["signal_scale", "length_scale"], self.kernel_config
+        )
+
+        s = self.kernel_config["signal_scale"]
+        l = self.kernel_config["length_scale"]
+
+        scaled_dist = jnp.sqrt(3.) * jnp.sqrt(self._sq_dist(x, y)) / l
+
+        normaliser = 1 + scaled_dist
+        exponential_term = jnp.exp(-scaled_dist)
+        return (s**2) * normaliser * exponential_term
+
+
+    def omega_fn(self, key: chex.PRNGKey, num_input_dims: int, num_features: int):
+        return jr.t(key, df=3., shape=(num_input_dims, num_features))
