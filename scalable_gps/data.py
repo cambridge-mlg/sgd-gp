5e160a439ede255bd173f4292da05bc66827dfbc
shreyaspadhy <shreyaspadhy@gmail.com>
2023-03-29 13:06:07 +0000

    changes for toy plots

--- scalable_gps/data.py
+++ scalable_gps/data.py
@@ -44,7 +44,7 @@ def get_concentrating_toy_sin_dataset(
     signal = f(x)
     y = signal + jr.normal(k2, shape=signal.shape) * noise_scale
 
-    x_test = jnp.linspace(-5.1, 5.1, n_test).reshape(-1, 1)
+    x_test = jnp.linspace(-10.1, 10.1, n_test).reshape(-1, 1)
     y_test = f(x_test)
 
     train_ds = Dataset(x, y, n, 1)
@@ -76,7 +76,7 @@ def get_expanding_toy_sin_dataset(
     signal = f(x)
     y = signal + jr.normal(k2, shape=signal.shape) * noise_scale
 
-    x_test = jnp.linspace(-(1.5) * n / n_periods, (1.5) * n / n_periods, n_test).reshape(-1, 1)
+    x_test = jnp.linspace(-5.1, 5.1, n_test).reshape(-1, 1)
     y_test = f(x_test)
 
     train_ds = Dataset(x, y, n, 1)
