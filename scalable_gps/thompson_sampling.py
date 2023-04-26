8742b7366fd923bc0989fdeb3ac421d1ec86a196
Jihao Andreas Lin <jal232@cam.ac.uk>
2023-04-26 15:40:38 +0000

    thompson sampling demo

--- scalable_gps/thompson_sampling.py
+++ scalable_gps/thompson_sampling.py
@@ -1,12 +1,12 @@
 import jax
 import jax.numpy as jnp
 import jax.random as jr
+import optax
 from chex import Array, PRNGKey
-from typing import Callable, Optional
+from typing import Callable
 
 from scalable_gps.kernels import Kernel
 from scalable_gps.data import Dataset
-from scalable_gps.linalg_utils import KvP
 
 
 def init(
@@ -76,12 +76,65 @@ def find_friends(
     return friends
 
 
-def get_posterior_sample_fn(w, alpha_sample, feature_fn, kernel_fn, ds, alpha_map, batch_size=1):
+def find_homies(
+        x_friends: Array,
+        y_friends: Array,
+        n_homies: int):
+    idx = jnp.argsort(-y_friends, axis=-1)[:, :n_homies]
+    return x_friends[idx] # [n_samples, n_homies, D]
 
+
+def find_besties(
+        x_homies: Array,
+        acquisition_fn: Callable,
+        n_besties: int = 1):
+    y_homies = acquisition_fn(x_homies)[0]
+    
+    @jax.vmap
+    def argsort(x, y):
+        return x[jnp.argsort(-y)[:n_besties]]
+    return argsort(x_homies, y_homies)
+
+def optimise_homies(
+        x_homies: Array,
+        acquisition_fn: Callable,
+        learning_rate: float = 1e-3,
+        iterations: int = 100):
+    # optimiser = LBFGS(acquisition_fn, value_and_grad=True, maxiter=maxiter, tol=tol)
+    optimiser = optax.adam(learning_rate=learning_rate)
+    
     @jax.jit
-    def f(x):
-        L = feature_fn(x)
-        return L @ w + KvP(x, ds.x, alpha_map - alpha_sample, kernel_fn=kernel_fn, batch_size=batch_size)
+    def update(x, opt_state):
+        value, grad = acquisition_fn(x)
+        # pass -grad to maximise function
+        updates, opt_state = optimiser.update(-grad, opt_state, x)
+        x = optax.apply_updates(x, updates)
+        return x, opt_state, value
+    
+    opt_state = optimiser.init(x_homies)
+    trace = []
+    for _ in range(iterations):
+        x_homies, opt_state, y_homies = update(x_homies, opt_state)
+        trace.append((x_homies, y_homies))
+    return x_homies, trace
 
-    return f
+
+def get_acquisition_fn(ds, alpha_map, alpha_samples, w_samples, feature_fn, kernel_fn, **kernel_kwargs):
+
+    def _fn(x, alpha_sample, w_sample):
+        # x: (D,)
+        # alpha_sample: (n_train,)
+        # w_sample: (n_features,)
+        # return: ()
+        return (feature_fn(x) @ w_sample + kernel_fn(x, ds.x, **kernel_kwargs) @ (alpha_map - alpha_sample)).squeeze()
+
+    def _partial_vmapped_value_and_grad_fn(x):
+        # x: (n_samples, n_test, D)
+        # alpha_samples: (n_samples, n_train)
+        # w_samples: (n_samples, n_features)
+        # 1. vmap over 'n_test' for x only, 2. vmap over n_samples
+        # return: (n_samples, n_test)
+        return jax.jit(jax.vmap(jax.vmap(jax.value_and_grad(_fn), in_axes=(0, None, None))))(x, alpha_samples, w_samples)
+    
+    return _partial_vmapped_value_and_grad_fn
     
