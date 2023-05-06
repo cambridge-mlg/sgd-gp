e97b711e2b08e5dcb1c298c9dc3f86f514c88ad2
shreyaspadhy <shreyaspadhy@gmail.com>
2023-05-06 17:40:46 +0000

    adding name for final exps

--- scalable_gps/tpu/tpu_single_device_sweep.py
+++ scalable_gps/tpu/tpu_single_device_sweep.py
@@ -7,5 +7,5 @@ for dataset in datasets:
     for method in methods:
         for split in splits:
             name = f"{dataset}_{method}_{split}"
-            command = f"python scripts/obtain_mean.py 9835d6db89010f73306f92bb9a080c9751b25d28 --config configs/default.py:{dataset} --config.model_name {method} --config.wandb.log --config.dataset_config.split {split} --config.wandb.name {name}"
+            command = f"python scripts/obtain_mean.py 9835d6db89010f73306f92bb9a080c9751b25d28 --config configs/default.py:{dataset} --config.model_name {method} --config.wandb.log --config.dataset_config.split {split} --config.wandb.name final_{name}"
             subprocess.run(command.split())
