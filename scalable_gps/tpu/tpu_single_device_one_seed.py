2179944fafb5f33dab3b56b35d38e667a803c592
shreyaspadhy <shreyaspadhy@gmail.com>
2023-05-07 23:35:56 +0000

    adding nocompute exact soln for big datasets

--- scalable_gps/tpu/tpu_single_device_one_seed.py
+++ scalable_gps/tpu/tpu_single_device_one_seed.py
@@ -9,7 +9,7 @@ for dataset in datasets:
     for method in methods:
         for split in splits:
             name = f"{dataset}_{method}_{split}"
-            command = f"python scripts/obtain_mean.py 9835d6db89010f73306f92bb9a080c9751b25d28 --config configs/default.py:{dataset} --config.model_name {method} --config.wandb.log --config.dataset_config.split {split}"
+            command = f"python scripts/obtain_mean.py 9835d6db89010f73306f92bb9a080c9751b25d28 --config configs/default.py:{dataset} --config.model_name {method} --config.wandb.log --config.dataset_config.split {split} --noconfig.compute_exact_soln"
             
             name = f'debug_sweep_{name}'
             if low_noise:
