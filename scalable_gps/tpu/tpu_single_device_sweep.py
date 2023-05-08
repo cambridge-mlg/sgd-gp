76e6f9d1f062d1aa375adb8ac295b9505a959d8d
shreyaspadhy <shreyaspadhy@gmail.com>
2023-05-08 21:09:48 +0000

    vi 6 datasets low noise

--- scalable_gps/tpu/tpu_single_device_sweep.py
+++ scalable_gps/tpu/tpu_single_device_sweep.py
@@ -5,7 +5,7 @@ import subprocess
 methods = ["vi"]
 splits = list(range(10))
 datasets = ['pol', 'elevators', 'bike', 'kin40k', 'protein', 'keggdirected']
-low_noise = False
+low_noise = True
 for dataset in datasets:
     for method in methods:
         for split in splits:
