e8eef229cba4119f35da82080991180f684f2f91
shreyaspadhy <shreyaspadhy@gmail.com>
2023-05-08 21:27:43 +0000

    cg 6 datasets low noise

--- scalable_gps/tpu/tpu_single_device_sweep.py
+++ scalable_gps/tpu/tpu_single_device_sweep.py
@@ -2,7 +2,7 @@ import subprocess
 
 # methods = ['sgd', 'cg', 'precondcg', 'vi']
 # methods = ['sgd', 'cg']
-methods = ["vi"]
+methods = ["cg"]
 splits = list(range(10))
 datasets = ['pol', 'elevators', 'bike', 'kin40k', 'protein', 'keggdirected']
 low_noise = True
@@ -10,7 +10,7 @@ for dataset in datasets:
     for method in methods:
         for split in splits:
             name = f"{dataset}_{method}_{split}"
-            command = f"python scripts/obtain_mean.py 9835d6db89010f73306f92bb9a080c9751b25d28 --config configs/default.py:{dataset} --config.model_name {method} --config.wandb.log --config.dataset_config.split {split} --noconfig.compute_exact_soln  --config.wandb.log_artifact --config.vi_config.learning_rate 1e-4"
+            command = f"python scripts/obtain_mean.py 9835d6db89010f73306f92bb9a080c9751b25d28 --config configs/default.py:{dataset} --config.model_name {method} --config.wandb.log --config.dataset_config.split {split} --noconfig.compute_exact_soln  --config.wandb.log_artifact"
             name = f'final_{name}'
             if low_noise:
                 name += '_low_noise'
