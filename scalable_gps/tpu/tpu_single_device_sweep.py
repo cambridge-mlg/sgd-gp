ab3fc3aaa592b797d76c652afb6e385e01243513
shreyaspadhy <shreyaspadhy@gmail.com>
2023-05-08 01:32:24 +0000

    minor fix

--- scalable_gps/tpu/tpu_single_device_sweep.py
+++ scalable_gps/tpu/tpu_single_device_sweep.py
@@ -11,7 +11,7 @@ for dataset in datasets:
         for split in splits:
             name = f"{dataset}_{method}_{split}"
             command = f"python scripts/obtain_mean.py 9835d6db89010f73306f92bb9a080c9751b25d28 --config configs/default.py:{dataset} --config.model_name {method} --config.wandb.log --config.dataset_config.split {split} --noconfig.compute_exact_soln"
-            name = f'debug_sweep_{name}'
+            name = f'final_{name}'
             if low_noise:
                 name += '_low_noise'
                 command += " --config.override_noise_scale 0.001"
