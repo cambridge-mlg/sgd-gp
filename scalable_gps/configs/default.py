66fac91f9ec26213957814498b02fdb4a8eb98fa
Javier Antoran <javir803@gmail.com>
2023-05-12 16:30:58 +0100

    added new options to default config

--- scalable_gps/configs/default.py
+++ scalable_gps/configs/default.py
@@ -30,7 +30,7 @@ def get_config(config_string):
 
     config.compute_exact_soln = True
     config.use_tpu = True
-    config.override_noise_scale = -1.
+    config.override_noise_scale = -1.0
 
     config.model_name = "sgd"
     # Data Configs
@@ -46,16 +46,20 @@ def get_config(config_string):
     config.kernel_name = "Matern32Kernel"
     config.kernel_config = ml_collections.ConfigDict()
     config.kernel_config.use_ard = True
-    config.kernel_config.signal_scale = 1.
-    
-    length_scale_dim = config.dataset_config.input_dim if config.kernel_config.use_ard else 1
-    config.kernel_config.length_scale = length_scale_dim * [1.]
+    config.kernel_config.signal_scale = 1.0
+
+    length_scale_dim = (
+        config.dataset_config.input_dim if config.kernel_config.use_ard else 1
+    )
+    config.kernel_config.length_scale = length_scale_dim * [1.0]
 
     config.vi_config = ml_collections.ConfigDict()
     config.vi_config.iterations = 10000
     config.vi_config.batch_size = 100
     config.vi_config.num_inducing_points = 1024
     config.vi_config.inducing_init = "kmeans"
+    config.vi_config.annoy_pre_clustering = False
+    config.vi_config.clustering_length_scale_ratio = 1.0
     config.vi_config.learning_rate = 1e-3
     config.vi_config.absolute_clipping = 0.1
     config.train_config = ml_collections.ConfigDict()
@@ -64,12 +68,12 @@ def get_config(config_string):
     config.train_config.iterations = 100000
     config.train_config.batch_size = 512
     config.train_config.eval_every = 1000
-    config.train_config.time_budget_in_seconds = 0.
-    config.train_config.eval_every_in_seconds = 0.
+    config.train_config.time_budget_in_seconds = 0.0
+    config.train_config.eval_every_in_seconds = 0.0
     # RFF Configs
     config.train_config.n_features_optim = 100
     config.train_config.recompute_features = True
-    
+
     # Optimisation Configs
     config.train_config.iterative_idx = True
     config.train_config.learning_rate = 5e-1
@@ -79,7 +83,7 @@ def get_config(config_string):
     config.train_config.polyak = 100 / config.train_config.iterations
     config.train_config.absolute_clipping = 0.1  # -1 to avoid clipping
 
-    config.train_config.lr_schedule_name = None # "linear_schedule"
+    config.train_config.lr_schedule_name = None  # "linear_schedule"
     config.train_config.lr_schedule_config = ml_collections.ConfigDict()
 
     if config.train_config.lr_schedule_name == "linear_schedule":
@@ -87,9 +91,10 @@ def get_config(config_string):
         config.train_config.lr_schedule_config.transition_steps = int(
             config.train_config.iterations * 0.95
         )  # I set this to N steps * 0.75
-        config.train_config.lr_schedule_config.end_value = config.train_config.learning_rate / 33
+        config.train_config.lr_schedule_config.end_value = (
+            config.train_config.learning_rate / 33
+        )
 
-    
     config.sampling_config = config.train_config.copy_and_resolve_references()
     config.sampling_config.n_samples = 16
     # Full-batch training configs that get passed
@@ -106,19 +111,19 @@ def get_config(config_string):
     config.sampling_config.recompute_features = True
     config.sampling_config.loss_objective = 2
     config.sampling_config.use_cholesky_prior_sample = False
-    
+
     config.sampling_config.absolute_clipping = 0.1  # -1 to avoid clipping
-    
+
     config.mll_config = ml_collections.ConfigDict()
     config.mll_config.learning_rate = 0.1
     config.mll_config.iterations = 300
     config.mll_config.eval_every = 1
-    
-    config.mll_config.init_length_scale = length_scale_dim * [0.]
 
-    config.mll_config.init_signal_scale = 0.
-    config.mll_config.init_noise_scale = 0.
-    
+    config.mll_config.init_length_scale = length_scale_dim * [0.0]
+
+    config.mll_config.init_signal_scale = 0.0
+    config.mll_config.init_noise_scale = 0.0
+
     config.mll_config.subsample_seed = 0
     config.mll_config.n_subsample = 10000
 
@@ -126,12 +131,12 @@ def get_config(config_string):
     config.cg_config.batch_size = 1
     config.cg_config.tol = 1e-2
     config.cg_config.maxiter = 1000
-    config.cg_config.atol = 0.
+    config.cg_config.atol = 0.0
     config.cg_config.eval_every = 1
     config.cg_config.preconditioner = True
     config.cg_config.pivoted_chol_rank = 100
     config.cg_config.pivoted_diag_rtol = 1e-3
-    config.cg_config.pivoted_jitter = 1.
+    config.cg_config.pivoted_jitter = 1.0
     config.cg_config.loss_objective = 2
 
     config.cg_sampling_config = ml_collections.ConfigDict()
@@ -140,12 +145,12 @@ def get_config(config_string):
     config.cg_sampling_config.n_samples = 16
     config.cg_sampling_config.tol = 1e-3
     config.cg_sampling_config.maxiter = 1000
-    config.cg_sampling_config.atol = 0.
+    config.cg_sampling_config.atol = 0.0
     config.cg_sampling_config.eval_every = 1
     config.cg_sampling_config.preconditioner = True
     config.cg_sampling_config.pivoted_chol_rank = 100
     config.cg_sampling_config.pivoted_diag_rtol = 1e-3
-    config.cg_sampling_config.pivoted_jitter = 1.
+    config.cg_sampling_config.pivoted_jitter = 1.0
     config.cg_sampling_config.loss_objective = 2
 
     # Wandb Configs
@@ -156,5 +161,5 @@ def get_config(config_string):
     config.wandb.code_dir = "/home/shreyaspadhy_gmail_com/scalable-gaussian-processes"
     config.wandb.name = ""
     config.wandb.log_artifact = False
-    
+
     return config
